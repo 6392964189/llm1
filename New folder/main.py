c53c789f03b299b4f87294ed5b0e7a51beeedb2e
shobhit srivastava <121626970+6392964189@users.noreply.github.com>
2024-01-14 10:11:32 -0800

    Add files via upload

---
+++ New folder/main.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+from langchain_helper import create_vector_db, get_qa_chain
+
+st.title("Shobhit QA" )
+btn=st.button("Create Kmowledgebase")
+if btn:
+    pass
+
+question=st.text_input("Question:")
+
+
+
+if question:
+    chain=get_qa_chain()
+    response=chain(question)
+
+
+    st.header("Answer: ")
+    st.write(response["result"])
+
+
+    
